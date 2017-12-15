9f8351acf5e9166fbe3c0b9fe40be0bc5fa89684
Jay Kim <jkim208@masonlive.gmu.edu>
2017-12-14 19:33:15 -0500

    Added users.js

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
 // App.js
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+
+// Init app variable with express
+const app = express();
+
+const users = require('./routes/users');
+
+// Port
+const port = 3115;
+
+// CORS Middleware
+app.use(cors());
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+
+app.use('/users', users);
+
+// Index Route
+app.get('/', (req, res) => {
+    res.send('Invalid Endpoint');
+});
+
+// Start Server
+app.listen(port, () => {
+    console.log('Server started on port ' + port);
+});
